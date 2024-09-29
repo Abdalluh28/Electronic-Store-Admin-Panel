7fcc3501dd6174d110896790c7a3d852d4415ca1
Abdalluh28 <Abdok7496@gmail.com>
2024-09-29 08:27:26 +0300

    cors edit

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://store-admin-panel-rbmq.vercel.app'
+    'https://store-admin-panel-rbmq.vercel.app',
+    'https://store-admin-panel-qn0szvms2-abdalluhs-projects.vercel.app',
+    'https://store-admin-panel-rbmq-63syjlsn0-abdalluhs-projects.vercel.app'
 ];
 
 module.exports = allowedOrigins;
