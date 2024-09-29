ad6695518d5710592df98bf7bd2690ffb58ea302
Abdalluh28 <Abdok7496@gmail.com>
2024-09-29 06:56:40 +0300

    another commit

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/one.html'));
+    res.sendFile(path.join(__dirname, '../views/first.html'));
 })
 
 
